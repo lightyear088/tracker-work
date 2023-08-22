228e2614e5ea72d573c735997684d909545011ff
lightyear088 <k2017k@mail.ru>
2023-08-22 09:50:42 +0300

    reale pack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, ChangeEvent} from 'react';
 import './App.css';
+import { start } from 'repl';
+import { Console } from 'console';
 
-function App() {
+
+export default function App() {
+
+  // переменные и состояния
+  const [inputTextName, setInputTextName] = useState("");
+  const [inputTextDiscription, setInputTextDiscription] = useState("");
+  const [Start, setStart] = useState({
+    mode: true,
+    text: "start"
+  });
+  
+
+
+  // обработчики
+
+  const handleClick = () => {
+    if (Start.text == "start") {
+      setStart({text: "stop", mode: false});
+    }
+    else
+    {
+      setStart({text: "start", mode: false});
+    }
+  };
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputTextName(e.target.value);
+  };
+  const handleChangeDiscription = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputTextDiscription(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Введите название задачи</h2>
+      <input onChange={handleChangeName}></input>
+      <h3>{inputTextName}</h3>
+      <h2>Введите описание задачи</h2>
+      <input onChange={handleChangeDiscription}></input>
+      <h3>{inputTextDiscription}</h3>
+
+      <div>
+      <button onClick={handleClick} className='StartTimer'>{Start.text}</button>
+      </div>
+
     </div>
   );
 }
-
-export default App;
